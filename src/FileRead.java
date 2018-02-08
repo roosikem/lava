aed3cd6996e629b665784b3c2003f41a029a637e
roosikem <manishprajapati92@gmail.com>
2018-02-08 17:31:01 +0530

    Update FileRead.java

--- src/FileRead.java
+++ src/FileRead.java
@@ -1,4 +1,10 @@
-package com.journaldev.hibernate.main;
+package com.example.web.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ReadFile implements Runnable{
 
@@ -15,6 +21,36 @@ public class ReadFile implements Runnable{
 	public void run() {
 		for(int i = start; i<end;i++){
 			System.out.println(directory[i]);
+			FileInputStream Fread=null;
+			 FileOutputStream Fwrite =null;
+			try {
+				File yourFile = new File("D:/Test2/"+directory[i]);
+				yourFile.createNewFile();
+				Fread = new FileInputStream("D:/Test/"+directory[i]);
+				Fwrite=new FileOutputStream(yourFile,false) ; 
+		            System.out.println("File is Copied"); 
+		            int c; 
+		            while((c=Fread.read())!=-1) 
+		            Fwrite.write((char)c); 
+		            Fread.close(); 
+		            Fwrite.close(); 
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}  catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				try {
+					Fread.close();
+					Fwrite.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+	           
+			}
+           
 		}
 		
 	}
